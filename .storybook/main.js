fb521c11a4f6b9611e0824a66cd6c67e4fee5e9e
devin-ai-integration <devin@midday.ai>
2024-07-03 14:12:36 +0000

    Configure Storybook builder and update lock file

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,4 +2,7 @@ module.exports = {
   stories: ['../packages/ui/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
+  core: {
+    builder: '@storybook/react-vite',
+  },
 };
